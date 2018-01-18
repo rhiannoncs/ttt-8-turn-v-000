911163aa112806376467cee0c8d90455a3daf81b
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-18 16:59:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
